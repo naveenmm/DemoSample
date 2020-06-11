edc1a5b9276f22465f952bd26cb58530d089c37f
Naveen Mm <naveen.mm@soti.net>
2020-06-11 17:30:12 +0530

    Add project files.

---
+++ DemoSample/Assets/index.js
@@ -0,0 +1,499 @@
+﻿var appDataModels = {
+    "metaData": {
+        "appId": "5",
+        "tenantId": 2186
+    },
+    "forms": {
+        "form1": {
+            "formDataGenyId": "form1",
+            "widgets": [
+                {
+                    "widgetDataType": "Jstring",
+                    "widgetType": "textbox",
+                    "widgetValue": "",
+                    "widgetDataGenyId": "textbox2"
+                },
+                {
+                    "widgetDataType": "Jstring",
+                    "widgetType": "textbox",
+                    "widgetValue": "",
+                    "widgetDataGenyId": "textbox7"
+                },
+                {
+                    "widgetDataType": "Jstring",
+                    "widgetType": "dropdown",
+                    "widgetValue": "",
+                    "widgetDataGenyId": "dropdown9"
+                },
+                {
+                    "widgetDataType": "Jstring",
+                    "widgetType": "flipswitch",
+                    "widgetValue": "",
+                    "widgetDataGenyId": "switch10"
+                },
+                {
+                    "widgetDataType": "Jstring",
+                    "widgetType": "radiobuttonlist",
+                    "widgetValue": "",
+                    "widgetDataGenyId": "radiobuttonlist11"
+                },
+                {
+                    "widgetDataType": "Jstring",
+                    "widgetType": "date",
+                    "widgetValue": "",
+                    "widgetDataGenyId": "date14"
+                }
+            ]
+        }
+    }
+};
+
+var appWidgetMetaData = {
+    "form1": {
+        "isCustomComputed": false,
+        "mapping": "mapsTo('form1')",
+        "relationType": "",
+        "decorator": "",
+        "formDataGenyId": "form1",
+        "formPageDataGenyId": "",
+        "parentWidgetDataGenyId": "",
+        "screenDataGenyId": "",
+        "widgetDataGenyId": "form1",
+        "widgetType": "form"
+    },
+    "page2": {
+        "isCustomComputed": false,
+        "mapping": "mapsTo('page2')",
+        "relationType": "",
+        "decorator": "",
+        "formDataGenyId": "form1",
+        "formPageDataGenyId": "page2",
+        "parentWidgetDataGenyId": "form1",
+        "screenDataGenyId": "",
+        "widgetDataGenyId": "page2",
+        "widgetType": "page"
+    },
+    "label1": {
+        "isCustomComputed": false,
+        "mapping": "mapsTo('label1')",
+        "relationType": "onetoone",
+        "decorator": "None",
+        "formDataGenyId": "form1",
+        "formPageDataGenyId": "page2",
+        "parentWidgetDataGenyId": "page2",
+        "screenDataGenyId": "",
+        "widgetDataGenyId": "label1",
+        "widgetType": "label"
+    },
+    "textbox2": {
+        "isCustomComputed": false,
+        "mapping": "mapsTo('textbox2')",
+        "relationType": "onetoone",
+        "decorator": "None",
+        "formDataGenyId": "form1",
+        "formPageDataGenyId": "page2",
+        "parentWidgetDataGenyId": "page2",
+        "screenDataGenyId": "",
+        "widgetDataGenyId": "textbox2",
+        "widgetType": "textbox"
+    },
+    "textbox7": {
+        "isCustomComputed": false,
+        "mapping": "mapsTo('textbox7')",
+        "relationType": "onetoone",
+        "decorator": "None",
+        "formDataGenyId": "form1",
+        "formPageDataGenyId": "page2",
+        "parentWidgetDataGenyId": "page2",
+        "screenDataGenyId": "",
+        "widgetDataGenyId": "textbox7",
+        "widgetType": "textbox"
+    },
+    "label8": {
+        "isCustomComputed": false,
+        "mapping": "mapsTo('label8')",
+        "relationType": "onetoone",
+        "decorator": "None",
+        "formDataGenyId": "form1",
+        "formPageDataGenyId": "page2",
+        "parentWidgetDataGenyId": "page2",
+        "screenDataGenyId": "",
+        "widgetDataGenyId": "label8",
+        "widgetType": "label"
+    },
+    "dropdown9": {
+        "isCustomComputed": false,
+        "mapping": "mapsTo('dropdown9')",
+        "relationType": "onetoone",
+        "decorator": "Json",
+        "formDataGenyId": "form1",
+        "formPageDataGenyId": "page2",
+        "parentWidgetDataGenyId": "page2",
+        "screenDataGenyId": "",
+        "widgetDataGenyId": "dropdown9",
+        "widgetType": "dropdown"
+    },
+    "switch10": {
+        "isCustomComputed": false,
+        "mapping": "mapsTo('switch10')",
+        "relationType": "onetoone",
+        "decorator": "None",
+        "formDataGenyId": "form1",
+        "formPageDataGenyId": "page2",
+        "parentWidgetDataGenyId": "page2",
+        "screenDataGenyId": "",
+        "widgetDataGenyId": "switch10",
+        "widgetType": "flipswitch"
+    },
+    "radiobuttonlist11": {
+        "isCustomComputed": false,
+        "mapping": "mapsTo('radiobuttonlist11')",
+        "relationType": "onetoone",
+        "decorator": "Json",
+        "formDataGenyId": "form1",
+        "formPageDataGenyId": "page2",
+        "parentWidgetDataGenyId": "page2",
+        "screenDataGenyId": "",
+        "widgetDataGenyId": "radiobuttonlist11",
+        "widgetType": "radiobuttonlist"
+    },
+    "date14": {
+        "isCustomComputed": false,
+        "mapping": "mapsTo('date14')",
+        "relationType": "onetoone",
+        "decorator": "None",
+        "formDataGenyId": "form1",
+        "formPageDataGenyId": "page2",
+        "parentWidgetDataGenyId": "page2",
+        "screenDataGenyId": "",
+        "widgetDataGenyId": "date14",
+        "widgetType": "date"
+    },
+    "map15": {
+        "isCustomComputed": false,
+        "mapping": "mapsTo('map15')",
+        "relationType": "",
+        "decorator": "",
+        "formDataGenyId": "form1",
+        "formPageDataGenyId": "page2",
+        "parentWidgetDataGenyId": "page2",
+        "screenDataGenyId": "",
+        "widgetDataGenyId": "map15",
+        "widgetType": "map"
+    },
+    "listView16": {
+        "isCustomComputed": false,
+        "mapping": "mapsTo('listView16')",
+        "relationType": "",
+        "decorator": "",
+        "formDataGenyId": "form1",
+        "formPageDataGenyId": "page2",
+        "parentWidgetDataGenyId": "page2",
+        "screenDataGenyId": "",
+        "widgetDataGenyId": "listView16",
+        "widgetType": "listview"
+    },
+    "layout3": {
+        "isCustomComputed": false,
+        "mapping": "mapsTo('layout3')",
+        "relationType": "",
+        "decorator": "",
+        "formDataGenyId": "form1",
+        "formPageDataGenyId": "page2",
+        "parentWidgetDataGenyId": "page2",
+        "screenDataGenyId": "",
+        "widgetDataGenyId": "layout3",
+        "widgetType": "layout"
+    },
+    "layoutcell4": {
+        "isCustomComputed": false,
+        "mapping": "mapsTo('layoutcell4')",
+        "relationType": "",
+        "decorator": "",
+        "formDataGenyId": "form1",
+        "formPageDataGenyId": "page2",
+        "parentWidgetDataGenyId": "layout3",
+        "screenDataGenyId": "",
+        "widgetDataGenyId": "layoutcell4",
+        "widgetType": "layoutcell"
+    },
+    "layoutcell5": {
+        "isCustomComputed": false,
+        "mapping": "mapsTo('layoutcell5')",
+        "relationType": "",
+        "decorator": "",
+        "formDataGenyId": "form1",
+        "formPageDataGenyId": "page2",
+        "parentWidgetDataGenyId": "layout3",
+        "screenDataGenyId": "",
+        "widgetDataGenyId": "layoutcell5",
+        "widgetType": "layoutcell"
+    },
+    "button6": {
+        "isCustomComputed": false,
+        "mapping": "mapsTo('button6')",
+        "relationType": "",
+        "decorator": "",
+        "formDataGenyId": "form1",
+        "formPageDataGenyId": "page2",
+        "parentWidgetDataGenyId": "layoutcell5",
+        "screenDataGenyId": "",
+        "widgetDataGenyId": "button6",
+        "widgetType": "button"
+    }
+};
+
+var appValidationRules = {
+    "page2": [],
+    "form1": []
+};
+
+var appValidationRulesInfo = {};
+
+var appLifeCycleWidgetsInfo = {
+    "appStartup": [
+        "map15"
+    ],
+    "onScreenLoad": {
+        "page2": [
+            "listView16"
+        ]
+    },
+    "onFormSave": {
+        "form1": []
+    }
+};
+
+var appVisibilityRules = [];
+
+var restApiModels = {};
+
+var appEventsModels = {
+    "button6_click": {
+        "eventId": "button6_click",
+        "orderedActions": [
+            {
+                "actionType": "Save",
+                "parameters": {
+                    "widgetDataGenyId": "button6"
+                },
+                "runNextActionOnFail": false
+            },
+            {
+                "actionType": "Sync",
+                "parameters": {
+                    "widgetDataGenyId": "button6"
+                },
+                "runNextActionOnFail": false
+            },
+            {
+                "actionType": "ResetForm",
+                "parameters": {
+                    "widgetDataGenyId": "button6"
+                },
+                "runNextActionOnFail": false
+            }
+        ]
+    }
+};
+
+var appVariableNameIdMappings = {};
+
+//to stop modal popup scrolling
+$(document).on('popupafteropen', '[data-role="popup"]', function (event, ui) {
+    $('body').css('overflow', 'hidden').on('touchmove', function (e) {
+        e.preventDefault();
+    });
+}).on('popupafterclose', '[data-role="popup"]', function (event, ui) {
+    $('body').css('overflow', 'auto').off('touchmove');
+});
+document.addEventListener('appReady', function () {
+
+    $('[data-geny-id=' + UtilityModule.getFirstScreenDataGenyId() + ']').trigger('pageshow');
+}, false);
+
+
+var appWidgetConfigurations = {
+    "label1": {
+        "widgetType": "label",
+        "widgetDataGenyId": "label1",
+        "widgetConfiguration": {
+            "text": "Favorite Restruants"
+        }
+    },
+    "textbox2": {
+        "widgetType": "textbox",
+        "widgetDataGenyId": "textbox2",
+        "widgetConfiguration": {
+            "placeholder": "",
+            "dataType": "text",
+            "numericConfiguration": null
+        }
+    },
+    "textbox7": {
+        "widgetType": "textbox",
+        "widgetDataGenyId": "textbox7",
+        "widgetConfiguration": {
+            "placeholder": "",
+            "dataType": "text",
+            "numericConfiguration": null
+        }
+    },
+    "label8": {
+        "widgetType": "label",
+        "widgetDataGenyId": "label8",
+        "widgetConfiguration": {
+            "text": "Favorite food"
+        }
+    },
+    "dropdown9": {
+        "widgetType": "dropdown",
+        "widgetDataGenyId": "dropdown9",
+        "widgetConfiguration": {
+            "options": {
+                "optionsData": [
+                    {
+                        "id": "5e041674-b77e-494b-8938-c3c0fa4c4337",
+                        "value": "Choice_MtHn",
+                        "text": "Biriyani",
+                        "name": "",
+                        "selected": false
+                    },
+                    {
+                        "id": "9697f301-a0b7-4164-bf5a-fe1e9e0ccb46",
+                        "value": "ZUML",
+                        "text": "Pasta",
+                        "name": "",
+                        "selected": false
+                    },
+                    {
+                        "id": "cb8d92bf-1550-4920-8c51-584429cf6c32",
+                        "value": "zT3x",
+                        "text": "Pizza",
+                        "name": "",
+                        "selected": false
+                    },
+                    {
+                        "id": "e9dc65a7-998b-4abc-b162-15e4a07da921",
+                        "value": "vTh5",
+                        "text": "Burger",
+                        "name": "",
+                        "selected": false
+                    }
+                ]
+            }
+        }
+    },
+    "radiobuttonlist11": {
+        "widgetType": "radiobuttonlist",
+        "widgetDataGenyId": "radiobuttonlist11",
+        "widgetConfiguration": {
+            "options": {
+                "optionsData": [
+                    {
+                        "id": "296f34c3-b284-410e-b539-6034f72954ff",
+                        "value": "RW8w",
+                        "text": "Pepsi",
+                        "name": "radiobuttonlist11",
+                        "selected": false
+                    },
+                    {
+                        "id": "1d24f826-1b84-41a3-a2ff-d9f52d36bbb6",
+                        "value": "GbWM",
+                        "text": "Coco Cola",
+                        "name": "radiobuttonlist11",
+                        "selected": false
+                    }
+                ]
+            }
+        }
+    },
+    "date14": {
+        "widgetType": "date",
+        "widgetDataGenyId": "date14",
+        "widgetConfiguration": {
+            "defaultDate": "",
+            "minDate": "",
+            "maxDate": ""
+        }
+    },
+    "map15": {
+        "widgetType": "map",
+        "widgetDataGenyId": "map15",
+        "widgetConfiguration": {
+            "settings": {
+                "zoomControl": false,
+                "mapTypeControl": false,
+                "scaleControl": false,
+                "streetViewControl": false,
+                "rotateControl": false,
+                "fullscreenControl": true,
+                "disableDefaultControls": true,
+                "zoomLevel": 10,
+                "defaultLocation": {
+                    "lat": 43.62099,
+                    "lng": -79.6741943
+                },
+                "useCluster": true
+            },
+            "markers": []
+        }
+    },
+    "listView16": {
+        "widgetEventConfiguration": [
+            {
+                "eventName": "edit",
+                "selectorElement": "li",
+                "args": [
+                    "0"
+                ],
+                "domEvent": "click"
+            }
+        ],
+        "ChildControlsInfo": [
+            {
+                "widgetType": null,
+                "widgetDataGenyId": null
+            },
+            {
+                "widgetType": null,
+                "widgetDataGenyId": null
+            }
+        ],
+        "widgetType": "listview",
+        "widgetDataGenyId": "listView16",
+        "widgetConfiguration": {
+            "screenDataGenyId": "",
+            "defaultRowCount": "15000",
+            "formDataGenyId": "0"
+        }
+    },
+    "page2": {
+        "widgetType": "page",
+        "widgetDataGenyId": "page2",
+        "widgetConfiguration": {
+            "displayName": "Lala"
+        }
+    }
+}
+
+
+var appWidgetTemplates = {
+    "dropdown9": {
+        "widgetDataGenyId": "dropdown9",
+        "template": "<option value='key-select-a-value'>Select food</option>{{#each optionsData}}{{#if selected}} <option value='{{value}}' selected>{{text}}</option>{{else}}<option value='{{value}}' >{{text}}</option> {{/if}}{{/each}}"
+    },
+    "radiobuttonlist11": {
+        "widgetDataGenyId": "radiobuttonlist11",
+        "template": "<div class='ui-controlgroup-controls'>{{#each optionsData}}{{#if selected}} <div class='ui-grid-a ui-responsive'><label for='{{id}}' class='ui-btn ui-corner-all ui-btn-inherit ui-btn-icon-left ui-radio-off ui-first-child madp-canvas-radio-filled'>{{text}}</label><input type='radio' name='{{name}}' id='{{id}}' data-geny='{{id}}' value='{{value}}' checked></div>{{else}}<div class='ui-grid-a ui-responsive'><label for='{{id}}' class='ui-btn ui-corner-all ui-btn-inherit ui-btn-icon-left ui-radio-off ui-first-child madp-canvas-radio-filled'>{{text}}</label><input type='radio' name='{{name}}' id='{{id}}' data-geny='{{id}}' value='{{value}}' ></div>{{/if}}{{/each}}</div>"
+    },
+    "listView16": {
+        "widgetDataGenyId": "listView16",
+        "template": "<tr id = '{{RecordId}}' data-rec-id ='{{RecordId}}' >  </tr>"
+    }
+};
+
+
+
+
+
